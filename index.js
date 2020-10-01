6a71621c741bb5414d1dc7de9dd03a1c325b43bd
ali-mohsin-works <alimohsin.works@gmail.com>
2020-10-01 14:26:00 -0400

    port correction

--- index.js
+++ index.js
@@ -16,5 +16,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server started at: http://52.86.154.61/:${PORT}`);
+  console.log(`Server started at: http://52.86.154.61:${PORT}`);
 });
