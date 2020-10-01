a2614fbb5366aa0800138ca37eecbb039dfa7069
ali-mohsin-works <alimohsin.works@gmail.com>
2020-10-01 12:57:27 -0400

    ssh configuration

---
+++ build/precache-manifest.1cd3509dadbdf6f0b43b145b723bd652.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc9ea3ecb3396a74c6560c377590312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c8aac9633ea25949263",
+    "url": "/static/js/2.7bcd0c9a.chunk.js"
+  },
+  {
+    "revision": "495b020832bcaf8a8db3",
+    "url": "/static/js/main.308907c7.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  }
+]);
--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
-const PORT = 3333;
+const PORT = 17391;
 
 app.use(express.urlencoded({ extended: false }));
 
 app.use(cors());
 
-app.use(require('./routes'));
+app.use(require("./routes"));
+app.use(express.static(`${__dirname}/../build`));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started at: http://localhost:${PORT}`);
+  console.log(`Server started at: http://52.86.154.61/:${PORT}`);
 });
